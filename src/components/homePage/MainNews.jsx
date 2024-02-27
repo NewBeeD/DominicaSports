188f2cc5b2f06a1204e724048fdc674900b22257
Danphil Daniel <danieldanphil@gmail.com>
2024-02-26 20:33:59 -0400

    Completed Slider

--- src/components/homePage/MainNews.jsx
+++ src/components/homePage/MainNews.jsx
@@ -2,12 +2,16 @@ import {  Box, Typography, Stack, Button, Skeleton } from '@mui/material'
 import { Link } from 'react-router-dom';
 
 
-import {slides} from '../../assets/imageData.json'
 import '../../css/MainNewsCss.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slide from '../../modules/MainHeadline/Slide';
 
 import { useState, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
+import Slider from 'react-slick';
 
 
 const MainNews = () => {
@@ -17,27 +21,40 @@ const MainNews = () => {
   let headline = articles && articles[0] ? articles.filter(item => item.headline == 'Yes' && item.league == 'DFA'): '';
 
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500, // Adjust the speed (in milliseconds) as needed
+    cssEase: 'cubic-bezier(.76,.49,.72,.66)',
+
+  };
+
+
 
   // const [headline, setHeadline] = useState(slides)
   const [newsCounter, setNewsCounter] = useState(0)
 
 
-  useEffect(()=>{ 
+  // useEffect(()=>{ 
 
-    const intervalId = setInterval(()=> {
+  //   const intervalId = setInterval(()=> {
 
     
-      setNewsCounter((prevCount) => {
+  //     setNewsCounter((prevCount) => {
 
-        if(prevCount < headline.length-1){return prevCount + 1}
-        else{return 0}
-      })
+  //       if(prevCount < headline.length-1){return prevCount + 1}
+  //       else{return 0}
+  //     })
       
-    }, 4000)
+  //   }, 4000)
 
-    // Clean up the interval when components unmounts
-    return () => clearInterval(intervalId)
-  }, [])
+  //   // Clean up the interval when components unmounts
+  //   return () => clearInterval(intervalId)
+  // }, [])
 
 
 
@@ -45,50 +62,20 @@ const MainNews = () => {
 
   return (
 
-    <Box height={{sm: 500}} sx={{ display: 'flex', justifyContent: 'center'}} marginTop={{xs: 0, sm: 4}}>
+    <div >
+      {headline.length > 0 ? <Slider {...settings}>
+        
+        {headline.map((slideData, idx) => (
+          <Slide key={idx} headline={slideData} />
+        ))}
 
-
-      {headline != '' ? <Box width={{xs: '100%', sm: '90%'}} height={{xs: 330, sm: 400}} sx={{ position: {xs: 'relative', sm: 'static'}}}>
-
-        <Link to={`/${headline[newsCounter].id}`}>
-          <img src={headline[newsCounter].url[0]} className='mainImage'/>
-        </Link>
-
-
-        <Box sx={{ position: {xs: 'absolute', sm: 'static'},  bottom: {xs: 18, sm: 'inherit'}, left: {xs: 12, sm: 'inherit'}, display: {sm: 'flex'}, justifyContent: {sm: 'center'} }}>
-
-          <Stack direction='column'>
-
-            <Box>
-              <Typography fontSize={{xs: 8, sm: 14}} sx={{ color: 'green', fontFamily: 'Josefin Slab', letterSpacing: 2, textAlign: {xs: 'none', sm: 'center', fontFamily: ''}}}>{headline[newsCounter].league}</Typography>
-            </Box>
-
-            <Box>
-              <Typography fontSize={{xs: 11, sm: 25}} sx={{ color: {xs:'white', sm: 'white'}}}>{headline[newsCounter].title}</Typography>
-            </Box>
-
-          </Stack>      
-
-        </Box>
-
-
-        {/* Horizontal Lines at bottom */}
-        <Stack direction='row' spacing={1} marginTop={{xs: 0, sm: 1}} sx={{  position: {xs: 'absolute', sm: 'static'}, bottom: {xs: 5, sm: 'inherit'}, left: {xs: 12, sm: 'inherit'}, display: {sm: 'flex'}, justifyContent: {sm: 'center'}}}>
-
-            {headline.map((_, idx) => {
-
-              return <Box key={idx} width={30} height={2} sx={{ backgroundColor: {xs: (newsCounter === idx? 'green': 'white'), sm: (newsCounter === idx? 'white': 'black')}}}></Box>
-
-            })}
-          
-        </Stack>  
-
-      </Box>: <Skeleton />}
+      </Slider>: <Skeleton />}
 
       
+    </div>
 
-      
-    </Box>
+
+
   )
 }
 
